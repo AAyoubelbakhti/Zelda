3c372f28a623ee9f2353fe322e75c3f6b7623150
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2024-01-10 16:55:55 +0100

    Menu con funciones vacias, miercoles
    Una idea de como va a ser el menu con las funciones vacias.

--- menu.py
+++ menu.py
@@ -1,4 +1,22 @@
 
 def menu():
     while True:
-        
+        clear_screen()
+        fondorandom()
+        accio = input("Select an option: ")
+        ac = accio.lower()
+        if ac == "continue":
+            if saved_games != None:
+                if len(saved_games) == 1:
+                    game()
+                else:
+                    saved_games_menu()
+        elif ac == "new game":
+            game()
+        elif ac == "help":
+            menu_help()
+        elif ac == "about":
+            about()
+        elif ac == "exit":
+            break
+  
