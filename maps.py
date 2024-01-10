adc647827f79a18bb01da18612eed2b6842d90a8
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2024-01-10 21:07:09 +0100

    Creación de los mapas
    Mapa Global funcional, habria que enlazar con los datos reales de que santuarios estan abiertos y realizar el resto de mapas.

---
+++ maps.py
@@ -0,0 +1,40 @@
+santuaris = ["S0 ","S1 ","S2 ","S3 ","S4?","S5?","S6?"]
+            
+maps = {
+    "global" : [ 
+        f"* Map * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
+        f"*                                                         *                   *",
+        f"*  Hyrule       {santuaris[0]}                       Death mountain  *                   *",
+        f"*                             {santuaris[2]}                         *                   *",
+        f"*        {santuaris[1]}                                       {santuaris[3]}    *                   *",
+        f"*                                                         *                   *",
+        f"*                          Castle                         *                   *",
+        f"*                                                         *                   *",
+        f"*                  {santuaris[4]}                              {santuaris[5]}   *                   *",
+        f"*  Gerudo                               {santuaris[6]}     Necluda   *                   *",
+        f"*                                                         *                   *",
+        f"* Back  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+
+    ],
+
+    "Hyrule" : [
+        
+    ],
+
+    "Death mountain" : [
+        
+    ],
+
+    "Gerudo" : [
+        
+    ],
+
+    "Necluda" : [
+        
+    ],
+
+    "Castle" : [
+        
+    ],
+
+}
