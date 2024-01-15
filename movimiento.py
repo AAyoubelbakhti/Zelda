d26682c28e7bcc657f33b12ebe0941fd9d63a6d1
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2024-01-15 21:00:36 +0100

    Esquema lo que hay que hacer de movimiento

---
+++ movimiento.py
@@ -0,0 +1,34 @@
+# La idea es hacer algo como Hyrule[3][4] y ir moviendose en un eje X Y.
+
+Lista de cosas que hacer
+
+- Santuarios ? a () y curan entero y + 1 max corazon + "Open Sanctuary"
+- Funcio Show map
+- Import Maps
+- Guardar la posicion del user 
+- Canvi seccio, santuari, menjar, get weapon = Save game
+- Las ! son la pos default del personaje
+- Los espacios se pueden atacar con la espada, 1/10 te da lagarto
+- O son piedras
+- C se puede cocinar al lado
+- T y TX.
+Puños: 40% poma, 10% espada madera o escudo madera
+Espada: 40% poma, 20% espada madera o escudo madera, Espada -1 uso 
+Arboles: 4 golpes muere durante 10 movimientos, X es el numero de movimientos left
+- ~ se obtiene pez 20%. Cuando se obtiene un pez, hasta que no se salga del mapa no respawnea. 
+- F solo se ve el 50% de las veces. Y se avisa si hay
+- X (LINK) go left X
+Go by X (Nom zona) - Recorrer toda la lista hasta el lugar mas cercano i guess
+go to Hyrule, anirà al ! ‘Hyrule’
+Hyrule X Necluda
+Death Mountain X Gerudo
+- M cofres
+Hyrule i Gerudo espases
+Death Mountain i Necluda escuts
+-W, un cofre obert
+Els cofres es tornen a tancar quan estan tots oberts, i el jugador es queda sense cap espasa
+- EX 
+Los enemigos tienen vida y cuando mueren desaparecen.
+Cuando atacas te quitan 1 corazon y se mueven 1 posicion
+- Castillo lo ultimo
+
