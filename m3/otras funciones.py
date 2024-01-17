070c3124189da19f3a8571b25c3c466d08889345
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-01-17 17:49:57 +0100

    Ordenación de archivos
    He separado los archivos en py más pequeños, reformulada la función  mostrar inventario y  eliminado prompt (ahora  lo tiene dani)

---
+++ m3/otras funciones.py
@@ -0,0 +1,23 @@
+import os
+import platform
+from diccionarios import inventory, item_counters
+
+
+#Función para limpiar la pantalla
+def clear_screen():
+    sistema = platform.system()
+    if sistema == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+#función para  el contador de blood moon (faltala parte para regenerar los  bichos)
+def decrease_blood_moon(inventory):
+    blood_moon = int(inventory['BloodMoon'])
+    if blood_moon > 0:
+        blood_moon -= 1
+        inventory['BloodMoon'] = str(blood_moon)
+    else:
+        inventory['BloodMoon'] = '25'
+
+
