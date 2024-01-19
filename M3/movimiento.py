256a2c3408412f113004682b03cc32da36dd0e40
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2024-01-19 18:26:20 +0100

    Funciones ATTACK Y OPEN

--- M3/movimiento.py
+++ M3/movimiento.py
@@ -4,7 +4,6 @@ from diccionarios import inventory, item_counters
 from comida import cook
 from inventario import generate_item
 from FuncionesPrompt import addToPrompt
-# Lista de cosas que hacer
 
 # - Canvi seccio, santuari, menjar, get weapon = Save game
 # go to Hyrule, anirà al ! ‘Hyrule’
@@ -27,112 +26,86 @@ open_chest = "W"
 enemy = "E"
 pos_link = maps["Hyrule"][6][11]
 Hyrule = maps["Hyrule"]
+
 def show_map(): #Esto muestra el mapa, pero tiene que cuadrar dentro de los *.
     for row in maps["global"]:
         print(row)
 
-def atk_gespa(current_position,mapa):
+def attack(ac,current_position,mapa):
+    words = ac.split()
+    blanc = words[1]
     x,y = current_position
-    if mapa(x+1,y) == gespa or mapa(x,y+1) == gespa or mapa(x,y-1) == gespa or mapa(x-1,y) == gespa:
-        lagarto = random.randint(1, 100)
-        if lagarto <= 10:
-            generate_item("meat")
-            addToPrompt("You got a Lizard.")
-        else:
-            addToPrompt("Nothing happened.")
+    if blanc == "grass":
+        if mapa(x+1,y) == gespa or mapa(x,y+1) == gespa or mapa(x,y-1) == gespa or mapa(x-1,y) == gespa:
+            lagarto = random.randint(1, 100)
+            if lagarto <= 10:
+                generate_item("meat")
+                addToPrompt("You got a Lizard.")
+            else:
+               addToPrompt("Nothing happened.")
+    elif blanc == "tree":
+            if mapa(x+1,y) == arbre or mapa(x,y+1) == arbre or mapa(x,y-1) == arbre or mapa(x-1,y) == arbre and tree_hp != 0:
+                if not espada_carregada:
+                    ob = random.randint(1,100)
+                    if ob <= 40:
+                        generate_item("vegetable")
+                        addToPrompt("You got an apple.")
+                    elif 40 < ob <= 50:
+                        ee = random.randint(0,1)
+                        if ee == 0:
+                            generate_item("wooden_sword")
+                            addToPrompt("You got a Wood sword.")
+                        else: 
+                            generate_item("wooden_shield")
+                            addToPrompt("You got a Wood shield.")
+                    else:
+                        addToPrompt("The tree didn't give you anything.")
+                else:
+                    ob = random.randint(1,100)
+                    if ob <= 40:
+                        generate_item("vegetable")
+                        addToPrompt("You got an apple.")
+                    elif 40 < ob <= 60:
+                        generate_item("wooden_sword")
+                        addToPrompt("You got a Wood sword.")
+                    elif 60 < ob <= 80:
+                        generate_item("wooden_shield")
+                        addToPrompt("You got a Wood shield.")
+                    else:
+                        addToPrompt("The tree didn't give you anything.")
+                    sword_hp = sword_hp - 1
+                    tree_hp = tree_hp - 1
+                    if tree_hp == 0:
+                        # Creo que habra que ponerle vidas a cada arbol, para asi meterle su contador
+    elif blanc == "fox":
 
-def sanctuary():
-# - Santuarios ? a () y curan entero y + 1 max corazon + "Open Sanctuary"
+    elif blanc == "enemy":
 
-def atk_tree(current_position,mapa):
+
+
+
+def fish(current_position, mapa):
     x,y = current_position
-    if mapa(x+1,y) == arbre or mapa(x,y+1) == arbre or mapa(x,y-1) == arbre or mapa(x-1,y) == arbre and tree_hp != 0:
-        if not espada_carregada:
-            ob = random.randint(1,100)
-            if ob <= 40:
-                generate_item("vegetable")
-                addToPrompt("You got an apple.")
-            elif 40 < ob <= 50:
-                ee = random.randint(0,1)
-                if ee == 0:
-                    generate_item("wooden_sword")
-                    addToPrompt("You got a Wood sword.")
-                else: 
-                    generate_item("wooden_shield")
-                    addToPrompt("You got a Wood shield.")
-            else:
-                addToPrompt("The tree didn't give you anything.")
+    if mapa(x+1,y) == agua or mapa(x,y+1) == agua or mapa(x,y-1) == agua or mapa(x-1,y) == agua:
+        peix = random.randint(1,100)
+        if peix <= 20:
+            generate_item("fish")
+            addToPrompt("You got a fish.")
+            fish_alive = False
         else:
-            ob = random.randint(1,100)
-            if ob <= 40:
-                generate_item("vegetable")
-                addToPrompt("You got an apple.")
-            elif 40 < ob <= 60:
-                generate_item("wooden_sword")
-                addToPrompt("You got a Wood sword.")
-            elif 60 < ob <= 80:
-                generate_item("wooden_shield")
-                addToPrompt("You got a Wood shield.")
-            else:
-                addToPrompt("The tree didn't give you anything.")
-            sword_hp = sword_hp - 1
-            tree_hp = tree_hp - 1
-            if tree_hp == 0:
-                # Creo que habra que ponerle vidas a cada arbol, para asi meterle su contador
-
-def fish():
-# - ~ se obtiene pez 20%. Cuando se obtiene un pez, hasta que no se salga del mapa no respawnea. 
-
-def fox():
-# - F solo se ve el 50% de las veces. Y se avisa si hay
-
-def chest():
-# - M cofres
-# Hyrule i Gerudo espases
-# Death Mountain i Necluda escuts
-# -W, un cofre obert
-    
-def enemy():
-# Los enemigos tienen vida y cuando mueren desaparecen.
-# Cuando atacas te quitan 1 corazon y se mueven 1 posicion
+            addToPrompt("You didn't get a fish.")
 
 
+def open(ac,current_position,mapa):
+    words = ac.split()
+    blanc = words[1]
+    x,y = current_position
 
+    if blanc == "sanctuary":
 
+    if blanc == "chest":
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
@@ -186,20 +159,18 @@ def movements(ac, mapa, current_position):
 
 
 
-
-
-
-
-
-
-
 def actions(ac, mapa, current_position):
-    if ac.split()[0] == "go":
+    words = ac.split()
+    action = words[0]
+    if action == "go":
         current_position = movements(ac, mapa, current_position)
     elif ac == "show map":
         show_map()
-    elif ac == "attack grass" and #Equipped Sword:
-        atk_gespa(current_position,mapa)
-    elif ac == "attack tree":
-        atk_tree(current_position,mapa)
+    elif action == "attack":
+        attack(ac,current_position,mapa)
+    elif action == "fish":
+        fish(current_position,mapa)
+    elif action == "open":
+        open(ac,current_position,mapa)
+        
     return current_position
